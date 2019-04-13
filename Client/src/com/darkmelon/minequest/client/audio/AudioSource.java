6aa6f92f3db33838aa991440a5a1e932d67ff00d
Adam <unconfigured@null.spigotmc.org>
2019-04-13 12:45:43 +0200

    Rewriting everything

--- Client/src/com/darkmelon/minequest/client/audio/AudioSource.java
+++
@@ -1,7 +0,0 @@
-package com.darkmelon.minequest.client.audio;
-
-public class AudioSource {
-
-	public int id;
-	public float x, y, z;
-}
--- Client/src/com/darkmelon/minequest/client/input/MouseButton.java
+++
@@ -1,6 +0,0 @@
-package com.darkmelon.minequest.client.input;
-
-public class MouseButton {
-
-	public static final int LEFT = 0, RIGHT = 1, MIDDLE = 2;
-}
